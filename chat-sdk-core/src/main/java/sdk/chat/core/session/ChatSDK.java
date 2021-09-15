e4739972dd179ed5e71a540bdf4fe3bd52903a46
Viktor De Pasquale <info@depasquale.wiki>
2021-09-15 14:24:20 +0200

    Fixed local database not being deleted on stop

--- chat-sdk-core/src/main/java/sdk/chat/core/session/ChatSDK.java
+++ chat-sdk-core/src/main/java/sdk/chat/core/session/ChatSDK.java
@@ -59,6 +59,7 @@ public class ChatSDK {
     public static ChatSDK shared() {
         return instance;
     }
+
     private static final ChatSDK instance = new ChatSDK();
 
     public static String Preferences = "chat_sdk_preferences";
@@ -81,17 +82,14 @@ public class ChatSDK {
 
     protected List<Runnable> onActivateListeners = new ArrayList<>();
 
-    protected ChatSDK () {
-    }
-
-    private void setContext (Context context) {
-        this.context = new WeakReference<>(context);
+    protected ChatSDK() {
     }
 
     /**
      * You can override the network adapter and interface adapter classes here. If these values are provided, they will be used instead of any that could
      * be provided by a module. These values can be null but by the end of setup, the network adapter and interface adapter must both be set. Either
      * here or by a module.
+     *
      * @param networkAdapterClass
      * @param interfaceAdapterClass
      * @return
@@ -104,11 +102,21 @@ public class ChatSDK {
     /**
      * Configure and let modules provide the interface and network adapters. We will loop over the modules and see if they provide each adapter,
      * the first that does will be used and any subsequent provider will be ignored.
+     *
      * @return
      */
     public static Config<ConfigBuilder<ChatSDK>> builder() {
-         shared().builder = new ConfigBuilder<>(shared());
+        shared().builder = new ConfigBuilder<>(shared());
         return shared().builder.builder();
+    }
+
+    /**
+     * Shortcut to return the interface adapter
+     *
+     * @return InterfaceAdapter
+     */
+    public static InterfaceAdapter ui() {
+        return shared().interfaceAdapter;
     }
 
     public void activate(Context context) throws Exception {
@@ -127,6 +135,180 @@ public class ChatSDK {
         activate(context, "Github:" + githubSponsorsId);
     }
 
+    public static CoreHandler core() {
+        return a().core;
+    }
+
+    public static AuthenticationHandler auth() {
+        return a().auth;
+    }
+
+    public static Context ctx() {
+        return shared().context();
+    }
+
+    public static ThreadHandler thread() {
+        return a().thread;
+    }
+
+    public static String getString(@StringRes int stringId) {
+        return ctx().getString(stringId);
+    }
+
+    public static Exception getException(@StringRes int stringId) {
+        return new Exception(ctx().getString(stringId));
+    }
+
+    public Context context() {
+        return context.get();
+    }
+
+    public static Config config() {
+        return shared().config;
+    }
+
+    public FileManager fileManager() {
+        return fileManager;
+    }
+
+    public static PublicThreadHandler publicThread() {
+        return a().publicThread;
+    }
+
+    public static PushHandler push() {
+        return a().push;
+    }
+
+    public static UploadHandler upload() {
+        return a().upload;
+    }
+
+    public static EventHandler events() {
+        return a().events;
+    }
+
+    public static User currentUser() {
+        return auth().currentUser();
+    }
+
+    public static SearchHandler search() {
+        return a().search;
+    }
+
+    public static ContactHandler contact() {
+        return a().contact;
+    }
+
+    public static BlockingHandler blocking() {
+        return a().blocking;
+    }
+
+    public static IEncryptionHandler encryption() {
+        return a().encryption;
+    }
+
+    public static LastOnlineHandler lastOnline() {
+        return a().lastOnline;
+    }
+
+    public static AudioMessageHandler audioMessage() {
+        return a().audioMessage;
+    }
+
+    public static String currentUserID() {
+        return auth().getCurrentUserEntityID();
+    }
+
+    public static VideoMessageHandler videoMessage() {
+        return a().videoMessage;
+    }
+
+    public static HookHandler hook() {
+        return a().hook;
+    }
+
+    public static StickerMessageHandler stickerMessage() {
+        return a().stickerMessage;
+    }
+
+    public static ContactMessageHandler contactMessage() {
+        return a().contactMessage;
+    }
+
+    public static FileMessageHandler fileMessage() {
+        return a().fileMessage;
+    }
+
+    public static ImageMessageHandler imageMessage() {
+        return a().imageMessage;
+    }
+
+    public static LocationMessageHandler locationMessage() {
+        return a().locationMessage;
+    }
+
+    public static ReadReceiptHandler readReceipts() {
+        return a().readReceipts;
+    }
+
+    public static TypingIndicatorHandler typingIndicator() {
+        return a().typingIndicator;
+    }
+
+    public static ProfilePicturesHandler profilePictures() {
+        return a().profilePictures;
+    }
+
+    public static StorageManager db() {
+        return shared().storageManager;
+    }
+
+    public static String getMessageImageURL(Message message) {
+        String imageURL = message.getImageURL();
+        if (StringChecker.isNullOrEmpty(imageURL)) {
+            imageURL = imageMessage().getImageURL(message);
+        }
+        if (StringChecker.isNullOrEmpty(imageURL)) {
+            imageURL = locationMessage().getImageURL(message);
+        }
+        if (StringChecker.isNullOrEmpty(imageURL)) {
+            for (Module module : shared().builder.modules) {
+                if (module.getMessageHandler() != null) {
+                    imageURL = module.getMessageHandler().getImageURL(message);
+                    if (imageURL != null) {
+                        break;
+                    }
+                }
+            }
+        }
+        return imageURL;
+    }
+
+    public static String getMessageText(Message message) {
+        String text = message.isReply() ? message.getReply() : message.getText();
+        if (StringChecker.isNullOrEmpty(text)) {
+            text = imageMessage().toString(message);
+        }
+        if (StringChecker.isNullOrEmpty(text)) {
+            text = locationMessage().toString(message);
+        }
+        if (StringChecker.isNullOrEmpty(text)) {
+            for (Module module : shared().builder.modules) {
+                if (module.getMessageHandler() != null) {
+                    text = module.getMessageHandler().toString(message);
+                    if (!StringChecker.isNullOrEmpty(text)) {
+                        break;
+                    }
+                }
+            }
+        }
+        return text;
+    }
+
+    private void setContext(Context context) {
+        this.context = new WeakReference<>(context);
+    }
+
     public void activate(Context context, @Nullable String identifier) throws Exception {
 
         if (isActive) {
@@ -134,7 +316,7 @@ public class ChatSDK {
         }
 
         setContext(context);
-         keyStorage = new KeyStorage(context);
+        keyStorage = new KeyStorage(context);
 
         config = builder.config();
 
@@ -148,9 +330,9 @@ public class ChatSDK {
             Logger.info("Interface adapter provided by ChatSDK.configure call");
         }
 
-        for (Module module: builder.modules) {
+        for (Module module : builder.modules) {
             if (networkAdapter == null) {
-                if(module instanceof NetworkAdapterProvider) {
+                if (module instanceof NetworkAdapterProvider) {
                     NetworkAdapterProvider provider = (NetworkAdapterProvider) module;
                     if (provider.getNetworkAdapter() != null) {
                         networkAdapter = provider.getNetworkAdapter();
@@ -159,7 +341,7 @@ public class ChatSDK {
                 }
             }
             if (interfaceAdapter == null) {
-                if(module instanceof InterfaceAdapterProvider) {
+                if (module instanceof InterfaceAdapterProvider) {
                     InterfaceAdapterProvider provider = (InterfaceAdapterProvider) module;
                     if (provider.getInterfaceAdapter() != null) {
                         interfaceAdapter = provider.getInterfaceAdapter();
@@ -167,7 +349,7 @@ public class ChatSDK {
                     }
                 }
             }
-            for (String permission: module.requiredPermissions()) {
+            for (String permission : module.requiredPermissions()) {
                 if (!requiredPermissions.contains(permission)) {
                     requiredPermissions.add(permission);
                 }
@@ -215,7 +397,7 @@ public class ChatSDK {
 
         fileManager = new FileManager(context);
 
-        for (Module module: builder.modules) {
+        for (Module module : builder.modules) {
             module.activate(context);
             Logger.info("Module " + module.getName() + " activated successfully");
         }
@@ -242,7 +424,7 @@ public class ChatSDK {
                 }
         }), HookEvent.MessageReceived);*/
 
-        for (Runnable r: onActivateListeners) {
+        for (Runnable r : onActivateListeners) {
             r.run();
         }
 
@@ -252,6 +434,11 @@ public class ChatSDK {
     }
 
     public void stop() {
+        Context localContext = context.get();
+        if (localContext != null) {
+            localContext.deleteDatabase("andorid-chatsdk-database");
+        }
+
         context = null;
         config = new Config<>(this);
         if (networkAdapter != null) {
@@ -264,203 +451,27 @@ public class ChatSDK {
         AppBackgroundMonitor.shared().stop();
 
         if (builder != null) {
-            for (Module module: builder.modules) {
+            for (Module module : builder.modules) {
                 module.stop();
             }
         }
         isActive = false;
     }
 
-    public static Context ctx() {
-        return shared().context();
-    }
-
-    public SharedPreferences getPreferences() {
-        return  context.get().getSharedPreferences(Preferences, Context.MODE_PRIVATE);
-    }
-
-    public static String getString(@StringRes int stringId) {
-        return ctx().getString(stringId);
-    }
-
-    public static Exception getException(@StringRes int stringId) {
-        return new Exception(ctx().getString(stringId));
-    }
-
-    public Context context() {
-        return context.get();
-    }
-
-    public static Config config() {
-        return shared().config;
-    }
-
-    public FileManager fileManager() {
-        return fileManager;
-    }
-
-    /**
-     * Shortcut to return the interface adapter
-     * @return InterfaceAdapter
-     */
-    public static InterfaceAdapter ui () {
-        return shared().interfaceAdapter;
-    }
-
-    public void setInterfaceAdapter (InterfaceAdapter interfaceAdapter) {
-        shared().interfaceAdapter = interfaceAdapter;
-    }
-
-    public void setNetworkAdapter (BaseNetworkAdapter networkAdapter) {
-        shared().networkAdapter = networkAdapter;
-    }
-
-    public static CoreHandler core () {
-        return a().core;
-    }
-
-    public static AuthenticationHandler auth () {
-        return a().auth;
-    }
-
-    public static ThreadHandler thread () {
-        return a().thread;
-    }
-
-    public static PublicThreadHandler publicThread () {
-        return a().publicThread;
-    }
-
-    public static PushHandler push () {
-        return a().push;
-    }
-
-    public static UploadHandler upload () {
-        return a().upload;
-    }
-
-    public static EventHandler events () {
-        return a().events;
-    }
-
-    public static User currentUser () {
-        return auth().currentUser();
-    }
-
-    public static String currentUserID() {
-        return auth().getCurrentUserEntityID();
-    }
-
-    public static SearchHandler search () {
-        return a().search;
-    }
-
-    public static ContactHandler contact () {
-        return a().contact;
-    }
-
-    public static BlockingHandler blocking () {
-        return a().blocking;
-    }
-
-    public static IEncryptionHandler encryption () { return a().encryption; }
-
-    public static LastOnlineHandler lastOnline () {
-        return a().lastOnline;
-    }
-
-    public static AudioMessageHandler audioMessage () {
-        return a().audioMessage;
-    }
-
-    public static VideoMessageHandler videoMessage () {
-        return a().videoMessage;
-    }
-
-    public static HookHandler hook () {
-        return a().hook;
-    }
-
-    public static StickerMessageHandler stickerMessage () {
-        return a().stickerMessage;
-    }
-
-    public static ContactMessageHandler contactMessage () {
-        return a().contactMessage;
-    }
-
-    public static FileMessageHandler fileMessage () {
-        return a().fileMessage;
-    }
-
-    public static ImageMessageHandler imageMessage () {
-        return a().imageMessage;
-    }
-
-    public static LocationMessageHandler locationMessage () {
-        return a().locationMessage;
-    }
-
-    public static ReadReceiptHandler readReceipts () {
-        return a().readReceipts;
-    }
-
-    public static TypingIndicatorHandler typingIndicator () {
-        return a().typingIndicator;
-    }
-
-    public static ProfilePicturesHandler profilePictures () {
-        return a().profilePictures;
-    }
-
     public static BaseNetworkAdapter a() {
         return shared().networkAdapter;
     }
 
-    public static StorageManager db () {
-        return shared().storageManager;
+    public SharedPreferences getPreferences() {
+        return context.get().getSharedPreferences(Preferences, Context.MODE_PRIVATE);
     }
 
-    public static String getMessageImageURL(Message message) {
-        String imageURL = message.getImageURL();
-        if(StringChecker.isNullOrEmpty(imageURL)) {
-            imageURL = imageMessage().getImageURL(message);
-        }
-        if(StringChecker.isNullOrEmpty(imageURL)) {
-            imageURL = locationMessage().getImageURL(message);
-        }
-        if(StringChecker.isNullOrEmpty(imageURL)) {
-           for (Module module: shared().builder.modules) {
-                if (module.getMessageHandler() != null) {
-                    imageURL = module.getMessageHandler().getImageURL(message);
-                    if (imageURL != null) {
-                        break;
-                    }
-                }
-            }
-        }
-        return imageURL;
+    public void setInterfaceAdapter(InterfaceAdapter interfaceAdapter) {
+        shared().interfaceAdapter = interfaceAdapter;
     }
 
-    public static String getMessageText(Message message) {
-        String text = message.isReply() ? message.getReply() : message.getText();
-        if(StringChecker.isNullOrEmpty(text)) {
-            text = imageMessage().toString(message);
-        }
-        if(StringChecker.isNullOrEmpty(text)) {
-            text = locationMessage().toString(message);
-        }
-        if(StringChecker.isNullOrEmpty(text)) {
-            for (Module module: shared().builder.modules) {
-                if (module.getMessageHandler() != null) {
-                    text = module.getMessageHandler().toString(message);
-                    if (!StringChecker.isNullOrEmpty(text)) {
-                        break;
-                    }
-                }
-            }
-        }
-        return text;
+    public void setNetworkAdapter(BaseNetworkAdapter networkAdapter) {
+        shared().networkAdapter = networkAdapter;
     }
 
     public List<String> getRequiredPermissions() {
